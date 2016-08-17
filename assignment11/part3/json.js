c0f45bf5582b4fb677d9ee4a645bb9900e3e24bd
wndjnky1 <wndjnky1@gmail.com>
2016-08-17 11:31:55 -0700

    Add files via upload

---
+++ assignment11/part3/json.js
@@ -0,0 +1,27 @@
+{"windsurfers":[
+	{
+		"name":"Robby Naish",
+		"products":"Naish Sails, Kites, Stand-up paddleboards",
+		"claimtofame":"Robert Staunton Naish is one of the first athletes to have gained long-lasting international fame in the sport of windsurfing."
+	},
+	{
+		"name":"Laird Hamilton",
+		"products":"Golfboard, Laird Apparel, XPT, Laird Superfood",
+		"claimtofame":"World renowned waterman Laird Hamilton is known as the guiding genius of crossover board sports, and is largely considered the primary influence behind many surfing innovations, including tow-in surfing, stand-up paddle boarding, and hydrofoil boarding. Surfer Magazine has labeled Laird as the sport’s most complete surfer,displaying almost unnerving expertise in a multitude of disciplines, and flat out surfing’s biggest, boldest, bravest, and the best big wave surfer in the world today, bar none."
+	},
+	{
+		"name":"Bjorn Dunkerbeck",
+		"products":"Starboard, Severne, Juice Plus, Ocean/Earth, Slam Park, Dunkerbeck-Eyewear",
+		"claimtofame":"Björn Dunkerbeck (born 16 July 1969) is a professional windsurfer who has won Professional Windsurfers Association (PWA) Overall World Championships a record forty one times."
+	},
+	{
+		"name":"Jason Polakow",
+		"products":"JP-Australia, Neil Pryde, Oxbow",
+		"claimtofame":"Jason Polakow was born in Torquay, Australia, and is a true all-rounder when it comes to extreme sports. Before he switched to windsurfing and made his name as a legend on the water, Jason had already earned the title of Australian Junior Moto X Champion. But at just 18 years old, he established himself as the top windsurfer in Australia and headed off to the world stage, where he quickly rose to the top."
+	},
+	{
+		"name":"Levi Siver",
+		"products":"Red Bull,Quatro, Dakine, Goya, MFC ",
+		"claimtofame":"King of Style Levi Siver didn’t take to windsurfing right away – he was born in landlocked Idaho, after all – but once he found his calling, there was no stopping him from reaching the sport’s highest levels. He’s a contest veteran with many wins to his name, but his true passion lies in freestyle windsurfing, where he’s considered a pioneer who’s made his own mark on the sport."
+	}
+]}
